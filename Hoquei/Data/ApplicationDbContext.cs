2adfc29092045da921152767d3296703547eee41
Duarte Valente <aluno22180@ipt.pt>
2022-04-07 15:32:29 +0100

    jogadores v1

--- Hoquei/Data/ApplicationDbContext.cs
+++ Hoquei/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Hoquei.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -8,9 +9,15 @@ namespace Hoquei.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options){}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Jogador>().HasData(
+                new Jogador { Num_Fed = 1, Name = "Antonio Alberto", Num_Cam = 10, Data_Nasc = "25/12/2000", Alcunha = "Toni"}
+
+            );
         }
+        public DbSet<Jogador> Jogador { get; set; }
     }
 }
--- Hoquei/Startup.cs
+++ Hoquei/Startup.cs
@@ -30,10 +30,13 @@ namespace Hoquei
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddDatabaseDeveloperPageExceptionFilter();
 
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                    .AddRoles<IdentityRole>()
+                    .AddEntityFrameworkStores<ApplicationDbContext>();
+
             services.AddControllersWithViews();
         }
 
