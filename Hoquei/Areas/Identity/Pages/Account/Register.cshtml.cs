cec3fa1d3d08854b937d9024b816a4e082120de7
Gonçalo Sousa <aluno21874@ipt.pt>
2022-04-28 16:42:07 +0100

    Delete Finished

--- Hoquei/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Hoquei/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -76,14 +76,14 @@ namespace Hoquei.Areas.Identity.Pages.Account {
          [Display(Name = "Email")]
          public string Email { get; set; }
 
-         //[Required]
-         //[StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
-         //[DataType(DataType.Password)]
+         [Required]
+         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+         [DataType(DataType.Password)]
          [Display(Name = "Password")]
          public string Password { get; set; }
 
-         //[DataType(DataType.Password)]
-         //[Display(Name = "Confirm password")]
+         [DataType(DataType.Password)]
+         [Display(Name = "Confirm password")]
          [Compare("Password", ErrorMessage = "A password e a sua confirmação não correspondem.")]
          public string ConfirmPassword { get; set; }
 
