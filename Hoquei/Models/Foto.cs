5cfaa3d3030b6062af039957be443a957220a980
Gonçalo Sousa <aluno21874@ipt.pt>
2022-04-29 18:50:06 +0100

    Model and Controller Created

---
+++ Hoquei/Models/Foto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hoquei.Models
+{
+    public class Foto
+    {
+        /// <summary>
+        /// Id da Foto
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Nome do ficheiro com a fotografia do hotel
+        /// </summary>
+        public string Nome { get; set; }
+
+        /// <summary>
+        /// Path para a foto
+        /// </summary>
+        public string Path { get; set; }
+
+        /// <summary>
+        /// Descricao da foto
+        /// </summary>
+        public string Descricao { get; set; }
+
+    }
+}
