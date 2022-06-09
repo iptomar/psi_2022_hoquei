9da51991fcab484991445d936ad18b3b5ce6416e
duartevalente10 <80701736+duartevalente10@users.noreply.github.com>
2022-06-09 16:58:33 +0100

    v2

---
+++ Hoquei/Controllers/ClassificacoesController.cs
@@ -0,0 +1,26 @@
+﻿using Hoquei.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hoquei.Controllers
+{
+    public class ClassificacoesController : Controller
+    {
+        private readonly HoqueiDB _context;
+
+        public ClassificacoesController(HoqueiDB context)
+        {
+            _context = context;
+        }
+
+        // GET: Calssificacoes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Classificacoes.ToListAsync());
+        }
+    }
+}
